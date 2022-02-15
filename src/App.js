dbaf34f3f4762236c523849317bde7e12d6fb98f
Voyager56 <acroduke37@gmail.com>
2022-02-15 18:42:28 +0400

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+
+const getGithubUser = async (username) => {
+  const response = await fetch(`https://api.github.com/users/${username}`);
+  const data = await response.json();
+  return data;
+};
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    if (username) {
+      getGithubUser(username).then((data) => setUser(data));
+    } else {
+      setUser(null);
+    }
+  }, [username]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <h1>Github User Search</h1>
+      <form>
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </form>
+      {username.length === 0 ? (
+        <p>Please enter a username</p>
+      ) : user ? (
+        <a className="user" target="_blank" href={user.html_link}>
+          <img className="avatar" src={user.avatar_url} alt="avatar" />
+          <h1 className="username">{user.username}</h1>
         </a>
-      </header>
+      ) : (
+        <p>Please enter username</p>
+      )}
     </div>
   );
 }
